481ab1b10ad05eb7783e9fbaeeb77fcb66dd2a26
lucasnather <natherlucas@gmail.com>
2025-02-23 19:50:16 -0400

    feat: rodando servidor

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT || 8080
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World!'
+  })
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta http://localhost:${port}`)
+})
