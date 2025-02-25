ff8ad595809c3c8b8822fffcbd3672217cd448a5
lucasnather <natherlucas@gmail.com>
2025-02-25 17:46:25 -0400

    feat: entities

--- src/model/connection.js
+++ src/model/connection.js
@@ -1,6 +1,18 @@
+import 'dotenv/config'
 import { Sequelize } from 'sequelize'
 
-const sequelize = new Sequelize({
+const sequelize = new Sequelize(
+    process.env.POSTGRES_DATABASE, 
+    process.env.POSTGRES_USERNAME, 
+    process.env.POSTGRES_PASSWORD,
+{
     dialect: 'postgres',
-    
+    port: process.env.POSTGRES_PORT,
 })
+
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
